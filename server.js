388dc908533968f6748655e726a9eb211a382170
InvalidSunX <rval2359@gmail.com>
2026-05-24 07:39:59 -0400

    Establish skeleton

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Budget app running on port ${PORT}`);
+});
+
+
